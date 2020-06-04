f79a46843e92ffd17c92f96fc56dfd8e7eb4bab9
Żaneta Jażdżyk <zaneta.jazdzyk@gmail.com>
2020-06-04 20:41:31 +0200

    case study ;)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import { AppBar, Button, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import FilterVintageIcon from '@material-ui/icons/FilterVintage';
+// import {HeaderBox, MyBox, MyDarkLink, MyLightLink, StyledToolbar} from "../../css/styles";
+// import NavHeader from "../NavHeader";
 
 import './App.css';
 
+const menuItems = ["Strona główna", "Oferta", "Realizacje", "Galeria", "Kontakt"];
+
+const menuItemsButtons = menuItems.map(text => {
+  return (
+    <Button key={text}>
+      <Link to={`/${text.replace(/ +/g, "").replace("ł", "l").replace("ó", 'o')}`}>
+        {text}
+      </Link>
+    </Button>
+  );
+});
+
 function App() {
+  const [open, setOpen] = useState(false);
+  const mediaQuery = useMediaQuery("(max-width:600px)");
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const hamburger = (
+    <IconButton
+      edge="end"
+      color="inherit"
+      aria-label="menu"
+      onClick={handleDrawerOpen}
+    >
+      <MenuIcon />
+    </IconButton>
+  );
+
   return (
     <Router>
       <div>
-        <nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -25,17 +65,50 @@ function App() {
               <Link to="/users">Users</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
+        {/* <NavHeader/> */}
+        <AppBar style={{ backgroundColor: '#1ea366' }} position="static">
+          <Toolbar>
+            {mediaQuery ? hamburger : <div>{menuItemsButtons}</div>}
+            <Drawer open={open} onClose={handleDrawerClose}>
+              <List>
+                {menuItems.map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>
+                      {text === "Strona główna" ? (
+                        <FilterVintageIcon />
+                      ) : (
+                          <FilterVintageIcon />
+                        )}
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Link
+                        to={`/${text.replace(/ +/g, "").replace("ł", "l").replace("ó", 'o')}`}
+                        onClick={handleDrawerClose}
+                      >
+                        {text}
+                      </Link>
+                    </ListItemText>
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+          </Toolbar>
+        </AppBar>
 
         {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <div>About</div>
+          <Route path="/Stronaglowna">
+            <div>Stronaglowna</div>
           </Route>
-          <Route path="/users">
-            <div>Users</div>
+          <Route path="/Oferta">
+            <div>Oferta</div>
+          </Route>          
+          <Route path="/Realizacje">
+            <div>Realizacje</div>
           </Route>
+          
           {/** And what will happen if path="/" was first instead last? */}
           <Route path="/">
             <div>Home</div>
